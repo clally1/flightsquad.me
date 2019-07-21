79a29002a8ec5c2409e432bfaaf13566c3d187d4
Chris <clally1@babson.edu>
2019-07-21 09:00:18 -0400

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -8,7 +8,7 @@ module.exports = (api) => {
     // See: https://github.com/facebook/create-react-app/blob/7548281aa5a9096f09cd5c9447cb4c21fa96ed4d/packages/react-scripts/config/webpack.config.js#L413
     return require('babel-preset-moxy/end-project')(api, {
         modules: false,
-        react: true,
+        react: false,
         targets: ['browsers'],
     });
 };
